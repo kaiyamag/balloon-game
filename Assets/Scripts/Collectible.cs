2c111f8da7d0710bd23193ecef584e735db5ca5c
kaiyamag <kmagnuson24@wooster.edu>
2024-07-01 16:38:29 -0700

    Add value parameter to IncrementScore and apply Collectible.cs to all coin pattern prefabs

--- Assets/Scripts/Collectible.cs
+++ Assets/Scripts/Collectible.cs
@@ -2,18 +2,51 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+/*
+Defines the behaviour of collectible objects when collected.
+*/
 public class Collectible : MonoBehaviour
 {
+    /* Score value assigned to this collectible
+    */
+    public int value;
+
+    /* Stores a reference to this object's mesh renderer
+    */
+    private MeshRenderer mesh;
+    
+    /* Stores a reference to the player object that can trigger collection
+    */
+    private GameObject player;
+
+    /*
+    Initialize mesh renderer
+    */
+    void Start()
+    {
+        player = GameObject.Find("HotAirBalloon");
+        if (player == null)
+        {
+            Debug.LogError("Player/HotAirBalloon object not found!");
+        }
+        mesh = GetComponent<MeshRenderer>();
+        mesh.enabled = true;
+    }
+
+    /*
+    Handles collisions with the player object and increments the score by this collectible's value.
+    Hides collectible on collision.
+    */
     private void OnTriggerEnter(Collider collisionInfo)
     {
-        if (collisionInfo.gameObject.name == "HotAirBalloon")
+        if (collisionInfo.gameObject.name == player.name)
         {
             ScoreTracker st = collisionInfo.gameObject.GetComponent<ScoreTracker>();
             if (st != null)
             {
                 Debug.Log("Collision between " + collisionInfo.gameObject.name + " and " + gameObject.name);
-                st.IncrementScore();
-                gameObject.SetActive(false);
+                st.IncrementScore(value);
+                mesh.enabled = false;
             } else {
                 Debug.LogError("ScoreTracker for " + collisionInfo.gameObject.name + " is null!");    
             }
